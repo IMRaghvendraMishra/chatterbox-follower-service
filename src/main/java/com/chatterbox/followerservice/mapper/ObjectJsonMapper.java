9fc971fcd36e4870884db55dae5e1a21b1b176f8
rmishra <raghvendra.k.mishra@outlook.com>
2025-05-07 16:58:05 +0530

    Map user json response to User object

--- src/main/java/com/chatterbox/followerservice/mapper/ObjectJsonMapper.java
+++ src/main/java/com/chatterbox/followerservice/mapper/ObjectJsonMapper.java
@@ -1,6 +1,7 @@
 package com.chatterbox.followerservice.mapper;
 
 import com.chatterbox.followerservice.model.FollowRequest;
+import com.chatterbox.followerservice.model.User;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.log4j.Log4j2;
@@ -22,5 +23,14 @@ public class ObjectJsonMapper {
             log.error("Unable to map followRequest object to JSON");
         }
         return json;
+    }
+
+    public User jsonToUser(String json) {
+        try {
+            return mapper.readValue(json, User.class);
+        } catch (JsonProcessingException e) {
+            log.error("Unable to parse JSON to User object", e);
+            return null;
+        }
     }
 }
