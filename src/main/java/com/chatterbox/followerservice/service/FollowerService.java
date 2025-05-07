0c7d754c6cfff9cdeea6590335402baab0321f60
rmishra <raghvendra.k.mishra@outlook.com>
2025-05-07 15:56:29 +0530

    Clean FollowerService

--- src/main/java/com/chatterbox/followerservice/service/FollowerService.java
+++ src/main/java/com/chatterbox/followerservice/service/FollowerService.java
@@ -1,53 +1,34 @@
 package com.chatterbox.followerservice.service;
 
-import jakarta.annotation.PostConstruct;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
-import java.util.Set;
 
 @Service
+@RequiredArgsConstructor
 public class FollowerService {
 
-    @Autowired
-    private RedisStorageService storage;
+    private final RedisStorageService redisStorageService;
 
     public void followUser(String followerId, String followeeId) {
-        storage.follow(followerId,followeeId);
-
+        redisStorageService.follow(followerId, followeeId);
     }
 
     public void unfollowUser(String followerId, String followeeId) {
-        storage.unfollow(followerId,followeeId);
+        redisStorageService.unfollow(followerId, followeeId);
     }
 
     public List<String> getFollowers(String userId) {
-        return storage.getFollowers(userId).stream().toList();
-        //return new ArrayList<>(followersMap.getOrDefault(userId, getDummyFollowers()));
+        return new ArrayList<>(redisStorageService.getFollowers(userId));
     }
 
     public List<String> getFollowing(String userId) {
-        return storage.getFollowing(userId).stream().toList();
-        //return new ArrayList<>(followingMap.getOrDefault(userId, getDummyFollowing()));
+        return new ArrayList<>(redisStorageService.getFollowing(userId));
     }
 
-    private Set<String> getDummyFollowers() {
-        return new HashSet<>(Arrays.asList("follower1", "follower2", "follower3"));
+    public void getAllRedisValues() {
+        redisStorageService.printAllKeysAndValues();
     }
-
-    private Set<String> getDummyFollowing() {
-        return new HashSet<>(Arrays.asList("following1", "following2", "following3"));
-    }
-
-    public void getAllRedisValues()
-    {
-        storage.printAllKeysAndValues();
-    }
-
 }
